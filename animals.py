695dfde90b087e34e1ee0d61a44ab9fc87468dfe
Brink Alexander <brink-aleksandr@yandex.ru>
2021-03-09 11:15:00 +0300

    Initial commit

---
+++ animals.py
@@ -0,0 +1,8 @@
+def whale(sound):
+    return 'whale says {}'.format(sound)
+
+def bird(sound):
+    return 'Bird says {}'.format(sound)
+
+def horse(sound):
+    return 'Horse says {}'.format(sound)
---
+++ macro.py
@@ -0,0 +1,33 @@
+from animals import horse
+import animals
+import sys
+
+def cat(sound):
+    return 'Cat says {}'.format(sound)
+
+def dog(sound):
+    return 'Dog says {}'.format(sound)
+
+def monkey(sound):
+    return 'Monkey says {}'.format(sound)
+
+if hasattr(sys.modules[__name__], 'dog'):
+    res = getattr(sys.modules[__name__], 'dog')("Bow")
+    print(res)
+else:
+    print('Not found')
+
+lname = "cat"
+
+# Если функция находится в этом же файле
+res = globals()[lname]('Meou')
+print(res)
+
+gname = "horse"
+
+# Если функция находится в другом файле (animals например)
+res = getattr(animals, gname)("Igogogo")
+print(res)
+
+
+    
